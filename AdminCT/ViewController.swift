8f5d481c83a58ad89f0f24bba027ef58f7e5eb20
Amin <amin030296@gmail.com>
2020-08-19 22:43:48 -0400

    Initial Commit

---
+++ AdminCT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdminCT
+//
+//  Created by Aminjoni Abdullozoda on 8/19/20.
+//  Copyright © 2020 Aminjoni Abdullozoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
